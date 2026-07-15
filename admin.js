d7e8e93b2267d26b5d672341e5159b02452a5868
q41332000-spec <q41332000@gmail.com>
2026-07-15 11:30:28 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -10,7 +10,7 @@ const numberInput = document.getElementById("number");
 const button = document.querySelector("button");
 
 // 櫃台密鑰
-const ADMIN_KEY = "0731";
+const ADMIN_KEY = "777";
 
 // 資料庫位置
 const queueRef = ref(db, "queue");
