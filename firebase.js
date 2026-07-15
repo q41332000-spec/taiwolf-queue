5d57a1278ddd8cdac912577ca40039113bb5f2ec
q41332000-spec <q41332000@gmail.com>
2026-07-15 10:04:33 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
+// firebase.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBDyEH02iQZnpm3q7ebdJl5gyoqv3Um7hM",
@@ -8,10 +10,12 @@ const firebaseConfig = {
   projectId: "taiwolf-5a279",
   storageBucket: "taiwolf-5a279.firebasestorage.app",
   messagingSenderId: "154565480180",
-  appId: "1:154565480180:web:22400fd7bc383b7bbc18a1"
+  appId: "1:154565480180:web:22400fd7bc383b7bbc18a1",
+  measurementId: "G-S4X322VC93"
 };
 
 const app = initializeApp(firebaseConfig);
+
 const db = getDatabase(app);
 
 export { db };
