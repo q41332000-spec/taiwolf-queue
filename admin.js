1c495e607bcb7e664219e758d272e1a932773d3c
q41332000-spec <q41332000@gmail.com>
2026-07-15 10:00:31 +0800

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,47 @@
+import { db } from "./firebase.js";
+
+import {
+    ref,
+    set,
+    onValue
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
+
+
+const numberInput = document.getElementById("number");
+const button = document.querySelector("button");
+
+
+// 顯示目前號碼
+const queueRef = ref(db, "queue");
+
+onValue(queueRef, (snapshot)=>{
+
+    const data = snapshot.val();
+
+    if(data){
+        numberInput.value = data.number;
+    }
+
+});
+
+
+// 更新號碼
+button.onclick = ()=>{
+
+    const number = numberInput.value;
+
+    if(number){
+
+        set(queueRef,{
+
+            number:number,
+
+            time:new Date().toLocaleString("zh-TW")
+
+        });
+
+        alert("更新完成");
+
+    }
+
+};
